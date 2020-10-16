ac1b79a4e9b63975dd862bd29eff2a26b15c2b15
roal-yr <roal.yr@gmail.com>
2020-10-17 00:16:09 +0300

    Crude CLI UI (not curses). To be refactored later.

--- apps/game/src/game.rs
+++ apps/game/src/game.rs
@@ -1,4 +1,5 @@
 use codec::*;
+use colored::*;
 use constants_app::*;
 use coords::Index;
 use io_ops::decompress_to_memory;
@@ -6,6 +7,11 @@ use io_ops::toml::{options, strings};
 use std::path::Path;
 use ui::prompts;
 use units::translate;
+
+use io_ops::toml::palettes::biomes;
+use io_ops::writepng::from_hex;
+use std::io;
+use std::io::Write;
 
 pub fn start(
 	_options: &options::Stuff,
@@ -75,12 +81,13 @@ pub fn start(
 		//Common actions
 		"?".to_string(),
 		"q".to_string(),
+		"render surrounding".to_string(),
 	]
 	.to_vec();
 
 	//Main loop init
 	let map_size = lp.wi.map_size;
-	let yx = Index { map_size };
+	let xy = Index { map_size };
 	let mut x = 0;
 	let mut y = 0;
 
@@ -91,8 +98,8 @@ pub fn start(
 	//Main loop
 	loop {
 		//Coordinates 1D,  2D, height
-		//To match the +x: right, +y:up, it should be like this.
-		let index = yx.ind(y, x);
+		//+x is north, +y is east
+		let index = xy.ind(x, y);
 
 		//Read data
 		let temp = translate::get_abs(
@@ -236,6 +243,9 @@ pub fn start(
 		println!("{}", georeg_id_str);
 		println!("{}", river_str);
 
+		//temporary here
+		render_land(&lp, x, y, 13);
+
 		//Input handling
 		let mut input = prompts::new_line_io("", &ui_el.prompt2);
 		input = prompts::autocomplete(&input, &commands);
@@ -244,22 +254,22 @@ pub fn start(
 		//Movement directions
 		match input.as_str() {
 			"west" => {
-				x = x.saturating_sub(1);
+				y = y.saturating_sub(1);
 			}
 			"north" => {
-				y = y.saturating_add(1);
-				if y >= map_size {
-					y = map_size - 1;
-				}
-			}
-			"east" => {
 				x = x.saturating_add(1);
 				if x >= map_size {
 					x = map_size - 1;
 				}
 			}
+			"east" => {
+				y = y.saturating_add(1);
+				if y >= map_size {
+					y = map_size - 1;
+				}
+			}
 			"south" => {
-				y = y.saturating_sub(1);
+				x = x.saturating_sub(1);
 			}
 			&_ => {}
 		}
@@ -290,6 +300,18 @@ pub fn start(
 		//Common commands?
 		match input.as_str() {
 			"q" => return,
+			"render surrounding" => {
+				let render_size = prompts::new_line_io(
+					&gm_str.gm25,
+					&ui_el.prompt2,
+				)
+				.trim()
+				.parse::<usize>()
+				.unwrap_or(10);
+				println!("{}", &ui_el.separator2);
+				render_land(&lp, x, y, render_size);
+				println!("{}", &ui_el.separator2);
+			}
 			"?" => {
 				println!("{}", &gm_str.gm2);
 				//Make this better
@@ -299,4 +321,206 @@ pub fn start(
 			&_ => {}
 		}
 	}
+}
+
+//▒▒▒▒▒▒▒▒▒▒▒▒ MESSY CODE AHEAD ▒▒▒▒▒▒▒▒▒▒▒▒▒
+//Move to other module or lib
+pub fn render_land(
+	lp: &LayerPack,
+	center_x: usize,
+	center_y: usize,
+	render_size: usize,
+) {
+	let mut render_line = Vec::new();
+	let map_size = lp.wi.map_size;
+	let xy = Index { map_size };
+	let bi: biomes::Stuff = biomes::get();
+
+	for i in 0..render_size * 2 {
+		let shift_x: isize = i as isize - render_size as isize;
+		for j in (0..render_size * 2).rev() {
+			let shift_y: isize = j as isize - render_size as isize;
+
+			let x = (center_x as isize - shift_x) as isize;
+			let y = (center_y as isize - shift_y) as isize;
+
+			if (x >= 0)
+				&& (y >= 0) && ((x as usize) < map_size)
+				&& ((y as usize) < map_size)
+			{
+				let index = xy.ind(x as usize, y as usize);
+				let elev =
+					lp.topography.read(lp.topography.TERRAIN, index);
+				let water = lp
+					.topography
+					.read(lp.topography.WATERMASK, index);
+				let river_width =
+					lp.rivers.read(lp.rivers.WIDTH, index);
+				let biome = lp.biomes.read(index);
+
+				//Forgive me what comes below. I will fix it later.
+				let element = match river_width {
+					//⁓≃≅≈≊≋
+					1 => ".⁓".truecolor(70, 100, 145),
+					2 => "⁓⁓".truecolor(70, 100, 145),
+					3 => "⁓⩪".truecolor(70, 100, 145),
+					4 => "⩪⩪".truecolor(70, 130, 190),
+					5 => "⩪≃".truecolor(70, 130, 190),
+					6 => "≃≃".truecolor(70, 130, 190),
+					7 => "≃≊".truecolor(70, 150, 230),
+					8 => "≊≊".truecolor(70, 150, 230),
+					9 => "≊≋".truecolor(70, 150, 230),
+					10 => "≋≋".truecolor(70, 195, 230),
+					11 => "≋⩭".truecolor(70, 195, 230),
+					12 => "⩰⩰".truecolor(70, 195, 230),
+
+					_ => {
+						//put biome tiles where there is no river tile
+						let argb: Vec<u8> = match biome {
+							0 => from_hex(&bi.color_0),
+							1 => from_hex(&bi.color_1),
+							2 => from_hex(&bi.color_2),
+							3 => from_hex(&bi.color_3),
+							4 => from_hex(&bi.color_4),
+							5 => from_hex(&bi.color_5),
+							6 => from_hex(&bi.color_6),
+							7 => from_hex(&bi.color_7),
+							8 => from_hex(&bi.color_8),
+							9 => from_hex(&bi.color_9),
+							10 => from_hex(&bi.color_10),
+							11 => from_hex(&bi.color_11),
+							12 => from_hex(&bi.color_12),
+							13 => from_hex(&bi.color_13),
+							14 => from_hex(&bi.color_14),
+							15 => from_hex(&bi.color_15),
+							16 => from_hex(&bi.color_16),
+							17 => from_hex(&bi.color_17),
+							18 => from_hex(&bi.color_18),
+							19 => from_hex(&bi.color_19),
+							20 => from_hex(&bi.color_20),
+							21 => from_hex(&bi.color_21),
+							22 => from_hex(&bi.color_22),
+							23 => from_hex(&bi.color_23),
+							24 => from_hex(&bi.color_24),
+							25 => from_hex(&bi.color_25),
+							26 => from_hex(&bi.color_26),
+							27 => from_hex(&bi.color_27),
+							28 => from_hex(&bi.color_28),
+							29 => from_hex(&bi.color_29),
+							30 => from_hex(&bi.color_30),
+							31 => from_hex(&bi.color_31),
+							32 => from_hex(&bi.color_32),
+							33 => from_hex(&bi.color_33),
+							34 => from_hex(&bi.color_34),
+							35 => from_hex(&bi.color_35),
+							36 => from_hex(&bi.color_36),
+							37 => from_hex(&bi.color_37),
+
+							_ => from_hex(&bi.color_100),
+						};
+
+						match biome {
+							0 => "⁓⁓" //water
+								.on_truecolor(argb[1], argb[2], argb[3]),
+							1 => "⁓⁓" //water
+								.on_truecolor(argb[1], argb[2], argb[3]),
+							2 => "⁓⁓" //water
+								.on_truecolor(argb[1], argb[2], argb[3]),
+							3 => "⁓⁓" //water
+								.on_truecolor(argb[1], argb[2], argb[3]),
+							4 => "⁓⁓" //water
+								.on_truecolor(argb[1], argb[2], argb[3]),
+							5 => "⁛⁚" //desert
+								.truecolor(argb[1], argb[2], argb[3]),
+							6 => "⁛⁚" //desert
+								.truecolor(argb[1], argb[2], argb[3]),
+							7 => "⁛⁚" //desert
+								.truecolor(argb[1], argb[2], argb[3]),
+							8 => "⁛⁚" //desert
+								.truecolor(argb[1], argb[2], argb[3]),
+							9 => "██" //glacier
+								.truecolor(argb[1], argb[2], argb[3]),
+							10 => "⁖⁖" //barren tundra
+								.truecolor(argb[1], argb[2], argb[3]),
+							11 => "██" //mountain too
+								.truecolor(argb[1], argb[2], argb[3]),
+							12 => "██" //mountain too
+								.truecolor(argb[1], argb[2], argb[3]),
+							13 => "██" //mountain too
+								.truecolor(argb[1], argb[2], argb[3]),
+							14 => "██" //mountain too
+								.truecolor(argb[1], argb[2], argb[3]),
+							15 => "██" //mountain too
+								.truecolor(argb[1], argb[2], argb[3]),
+							16 => "⚵⚶" //grassland
+								.truecolor(argb[1], argb[2], argb[3]),
+							17 => "⚵⚶" //grassland
+								.truecolor(argb[1], argb[2], argb[3]),
+							18 => "⚵⚶" //grassland
+								.truecolor(argb[1], argb[2], argb[3]),
+							19 => "⚵⚶" //grassland
+								.truecolor(argb[1], argb[2], argb[3]),
+								//⥾⨡⸶⸷⸙⸙☘⍋⍙⌇⌄⌄…‴⏃
+							20 => "⥾⏃" //woodland
+								.truecolor(argb[1], argb[2], argb[3]),
+							21 => "⥾⏃" //woodland
+								.truecolor(argb[1], argb[2], argb[3]),
+							22 => "⥾⏃" //woodland
+								.truecolor(argb[1], argb[2], argb[3]),
+							23 => "⍋⏃" //forest
+								.truecolor(argb[1], argb[2], argb[3]),
+							24 => "⍋⏃" //forest
+								.truecolor(argb[1], argb[2], argb[3]),
+							25 => "⍋⏃" //forest
+								.truecolor(argb[1], argb[2], argb[3]),
+							26 => "⸙≃" //swamp
+								.truecolor(argb[1], argb[2], argb[3]),
+							27 => "⸙≃" //swamp
+								.truecolor(argb[1], argb[2], argb[3]),
+							28 => "⸙≃" //swamp
+								.truecolor(argb[1], argb[2], argb[3]),
+							29 => "☘⍋" //rainforest
+								.truecolor(argb[1], argb[2], argb[3]),
+							30 => "☘⍋" //rainforest
+								.truecolor(argb[1], argb[2], argb[3]),
+							31 => "☘⍋" //rainforest
+								.truecolor(argb[1], argb[2], argb[3]),
+							32 => "⥾⚶" //shrubland
+								.truecolor(argb[1], argb[2], argb[3]),
+							33 => "⥾⚶" //shrubland
+								.truecolor(argb[1], argb[2], argb[3]),
+							34 => "⥾⚶" //shrubland
+								.truecolor(argb[1], argb[2], argb[3]),
+							35 => "…⚵" //alpine grassland
+								.truecolor(argb[1], argb[2], argb[3]),
+							36 => "…⚵" //alpine grassland
+								.truecolor(argb[1], argb[2], argb[3]),
+							37 => "…⚵" //alpine grassland
+								.truecolor(argb[1], argb[2], argb[3]),
+
+							_ => "▓▓"
+								.truecolor(argb[1], argb[2], argb[3]),
+						}
+					}
+				};
+
+				//Highlight central tile to see current position
+				if ((x as usize) == center_x)
+					&& ((y as usize) == center_y)
+				{
+					render_line.push(element.reversed());
+				} else {
+					render_line.push(element);
+				}
+			} else {
+				render_line.push("██".truecolor(20, 20, 20));
+			}
+		} //for j
+		for item in render_line.iter() {
+			print!("{}", item);
+		}
+		print!("|");
+		println!();
+		render_line.clear();
+	} //for i
 }
