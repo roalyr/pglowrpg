0633febd8558f447bc7ce5bc1e542455c0321046
Roman <roal.yr@gmail.com>
2021-05-19 01:50:58 +0300

    Features: implementing entity system further on. Added .ron support, finetuned structure and made basic destructors

--- apps/dev_test_corner/src/lib.rs
+++ apps/dev_test_corner/src/lib.rs
@@ -1,5 +1,6 @@
 use colored::*;
 use deepsize::DeepSizeOf;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use unit_systems::coords::Index;
 
@@ -20,15 +21,14 @@ pub fn start() {
 	println!("{}", "DEV TESTING CORNER".red());
 	println!("{}", "START\n".blue());
 	//▒▒▒▒▒▒▒▒▒▒▒▒ START ▒▒▒▒▒▒▒▒▒▒▒▒▒
-
 	// DEFINING OBJECTS
 	// This is a standard set of parameters for a type.
-	#[derive(Debug, Clone, DeepSizeOf)]
+	#[derive(Debug, Clone, Serialize, Deserialize, DeepSizeOf)]
 	struct MaterialProperties {
 		property: String, // Standard stuff such as material density, strength, etc.
 	}
 	// This is all possible variations of the Material type.
-	#[derive(Debug, Clone, DeepSizeOf)]
+	#[derive(Debug, Clone, Serialize, Deserialize, DeepSizeOf)]
 	enum Material {
 		Bone(MaterialProperties),
 		Flesh(MaterialProperties),
@@ -37,77 +37,92 @@ pub fn start() {
 	}
 
 	// Another pair of definitions.
-	#[derive(Debug, Clone, DeepSizeOf)]
+	#[derive(Debug, Clone, Serialize, Deserialize, DeepSizeOf)]
 	struct CreatureProperties {
 		materials: Vec<Material>,
 		// Behavior, size, structure, etc.
 	}
-	#[derive(Debug, Clone, DeepSizeOf)]
-	enum CreatureType {
-		//Fish(CreatureProperties),
-		//Bird(CreatureProperties),
-		Animal(CreatureProperties),
+	#[derive(Debug, Clone, Serialize, Deserialize, DeepSizeOf)]
+	enum EntityType {
+		//Plant(PlantProperties),
+		Creature(CreatureProperties),
+	}
+	#[derive(Debug, Clone, Serialize, Deserialize, DeepSizeOf)]
+	struct EntityData {
+		// Everything that must be written in .ron
+		name: String,
+		entity_type: EntityType,
 	}
 	// Keep generic data in the header of the struct for ease of access.
-	#[derive(Debug, Clone, DeepSizeOf)]
-	struct Creature {
+	#[derive(Debug, Clone, Serialize, Deserialize, DeepSizeOf)]
+	struct UniqueEntity {
 		uid: u32,
 		x: u32,
 		y: u32,
-		creature_type: CreatureType,
+		data: EntityData,
 		// Associated items (inventory cache ID), etc.
 		// Thoughts.
 	}
 
+	// RON stuff. Basically a string from the preset file.
+	// Should match the EntityType enum above.
+	let data = r#"
+			EntityData(
+				name : "Creature",
+				entity_type : Creature(
+					CreatureProperties(
+						materials: [
+							Bone(
+								MaterialProperties(
+									property: "Strong"
+								)
+							),
+							Flesh(
+								MaterialProperties(
+									property: "Meaty"
+								)
+							)
+						]
+					)
+				)
+			)
+		"#;
+
+	let entity_from_file: EntityData = match ron::from_str(&data) {
+		Ok(f) => f,
+		Err(e) => {
+			println!("{}", e.to_string());
+			std::process::exit(0);
+		}
+	};
+
 	// MAKING NON-SPATIAL GLOBAL TABLE.
 	// Make init cap and total cap. Defines hasmap size.
 	// Must be less than max ID values which will be U32 max.
-	let creature_cap = 1_000_00;
+	let entity_cap = 1_000_00;
 	// ENTITY TABLE
-	let mut unique_creatures: HashMap<u32, Creature> =
-		HashMap::with_capacity(creature_cap);
+	let mut unique_entities: HashMap<u32, UniqueEntity> =
+		HashMap::with_capacity(entity_cap);
 
 	// Filling up creatures object from preset.
-	for uid in 0..creature_cap {
-		unique_creatures
-			// Here we can try to insert result from parting .ron.
-			.insert(
-				uid as u32,
-				Creature {
-					uid: uid as u32,
-					x: 0,
-					y: 0,
-					creature_type: CreatureType::Animal(CreatureProperties {
-						materials: vec![
-							Material::Bone(MaterialProperties {
-								property: "Tough".to_string(),
-							}),
-							Material::Flesh(MaterialProperties {
-								property: "Meaty".to_string(),
-							}),
-							Material::Skin(MaterialProperties {
-								property: "Pale".to_string(),
-							}),
-							Material::Hair(MaterialProperties {
-								property: "Long".to_string(),
-							}),
-						], //mats
-					}), //type
-				}, //creature
-			); //insert
+	// Make match sort to generate different kinds of entities
+	// according to their caps.
+	for uid in 0..entity_cap {
+		unique_entities.insert(
+			uid as u32,
+			UniqueEntity {
+				uid: uid as u32,
+				x: 0,
+				y: 0,
+				data: entity_from_file.clone(),
+			}, //creature
+		); //insert
 	}
-
-	// Check stuff.
-	let single_creature = unique_creatures[&123].clone();
-	let entry_size = single_creature.deep_size_of();
-	println!("{}", "SINGLE ENTRY CHECK".green());
-	println!("Entry size: {:?}", entry_size);
-	println!("Entry content: {:?}", single_creature);
 
 	// WORLD-RELATED DATA TABLES AND CACHES.
 	// Map - Vec<Cache_ID>,
-	// Creatures cache table - HashMap<Cache_ID, Vec<Creature_ID>,
-	// Creatures table - HashMap<Creature_ID, Creature>,
+	// Entitys cache table - HashMap<Cache_ID, Vec<Entity_ID>,
+	// Entitys table - HashMap<Entity_ID, Entity>,
 
 	// Making a non-spatial cache table to put specific creatures
 	// onto the map in specific locations.
@@ -115,7 +130,7 @@ pub fn start() {
 	let map_size = 4096;
 	let cache_table_size = map_size * map_size;
 	// CACHE MAP
-	let mut creatures_cache: HashMap<u32, Vec<u32>> =
+	let mut entities_cache: HashMap<u32, Vec<u32>> =
 		HashMap::with_capacity(cache_table_size);
 
 	// Now put the creatures at x, y coords:
@@ -124,23 +139,54 @@ pub fn start() {
 	let queue = vec![123, 667, 9986];
 	let xy = Index { map_size };
 	let index = xy.ind(x, y) as u32;
-	creatures_cache.insert(index, queue.clone());
+	entities_cache.insert(index, queue.clone());
 	// Update coords in their headers.
-	for creature_id in queue.iter() {
-		unique_creatures.get_mut(creature_id).unwrap().x = x as u32;
-		unique_creatures.get_mut(creature_id).unwrap().y = y as u32;
+	// Make proper match for error
+	for entity_id in queue.iter() {
+		unique_entities.get_mut(entity_id).unwrap().x = x as u32;
+		unique_entities.get_mut(entity_id).unwrap().y = y as u32;
 	}
 
 	// Now access the creatures in the given location:
-	let local_creatures = &creatures_cache[&index];
+	let local_entities = &entities_cache[&index];
 	println!("{}", "\nCHECKING ENTRIES IN THE LOCATION".green());
-	println!("Putting creatures: {:?}", local_creatures);
+	println!("Putting entities: {:?}", local_entities);
 	println!("At x: {}, y: {}, index: {}\n", x, y, index);
-	for creature_id in local_creatures.iter() {
-		println!("{:?}", unique_creatures[creature_id]);
+	for entity_id in local_entities.iter() {
+		// Destruct the entity. What should this return? How and when?
+		match unique_entities.get_mut(entity_id) {
+			Some(entity) => {
+				println!("UID: {}", entity.uid);
+				match &entity.data.entity_type {
+					// Have different destructors for different types.
+					EntityType::Creature(properties) => {
+						println!("Type: {:?}", &entity.data.name);
+						// Move it into creature destructor.
+						for material in properties.materials.iter() {
+							// Move into material destructor.
+							match material {
+								Material::Bone(properties) => {
+									println!("{:?}", properties);
+								}
+								Material::Flesh(properties) => {
+									println!("{:?}", properties);
+								}
+								Material::Skin(properties) => {
+									println!("{:?}", properties);
+								}
+								Material::Hair(properties) => {
+									println!("{:?}", properties);
+								}
+							}
+						}
+					}
+				}
+			}
+			None => {
+				println!("ERROR: No entity by uid: {}", entity_id);
+			}
+		}
 	}
-
-	// Then parsethe Creature object...
 
 	// Implement I/O functions for the cache map.
 
